e22f28aa56833171b1cd95fb45c732c3b31504c7
mrony <marie.rony@gmail.com>
2023-11-28 16:46:27 +0100

    started module04

---
+++ D04/Abstract_Classes_and_interfaces/poly2.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+
+
+// Abstract class - can not be instancied
+class ACharacter { // A for Abstract
+	
+	private:
+		std::string _name;
+	public:
+		virtual void	attack(std::string const & target) = 0; // " = 0" means no implementation of this method !
+		void			sayHello(std::string const & target);
+
+};
+
+class Warrior : public ACharacter
+{
+	public:
+		virtual void	attack(std::string const & target);
+};
+
+void ACharacter::sayHello(std::string const & target) {
+	
+	std::cout << "hello " << target << "!" << std::endl;
+}
+
+void Warrior::attack(std::string const & target) {
+
+	std::cout << "Attacks " << target << " with a sword!" << std::endl;
+}
+
+// class ICoffeMaker { //I for Interface (no attributes in Interfaces)
+
+// 	public:
+// 		virtual void	fillWaterTank(IWaterSource * src) = 0;
+// 		virtual ICoffee	makeCoffee(std::string const & type) = 0;
+// };
+
+int main () {
+
+	ACharacter*	a = new Warrior();
+
+	// This would ne be OK because ACharacter is Abstract
+	// ACharacter* b = new ACharacter();
+
+	a->sayHello("strudent");
+	a->attack("Roger");
+
+}
---
+++ D04/Sub-Typing_Polymorphism/poly1.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <iostream>
+
+class Character {
+	public:
+		virtual void	sayHello(std::string const & target);
+};
+
+class Warrior : public Character {
+	public:
+		virtual void	sayHello(std::string const & target);
+};
+
+class Mage : public Character {
+	public:
+		virtual void	sayHello(std::string const & target);
+};
+
+class Cat
+{
+	//[...]
+};
+
+void Character::sayHello(std::string const & target) {
+
+	std::cout << "hello " << target << " !" << std::endl;
+}
+
+void Warrior::sayHello(std::string const & target) {
+
+	std::cout << "F*** off " << target << ", I don't like you !" << std::endl;
+}
+
+void Mage::sayHello(std::string const & target) {
+
+	std::cout << "Abracadabra " << target << " !" << std::endl;
+}
+
+int main () {
+
+	// this is OK, obviously, Warrior IS a Warrior
+	Character*	a = new Warrior();
+
+	// this is also OK because Warrior IS a Character
+	Character*	b = new Warrior();
+	Character*	c = new Mage();
+
+	// this would NOT be ok because Character IS NOT a warrior
+	// (although they are related, and W IS-A C, the reverse is not true)
+	// Warrior*	c = new Character();
+
+	// This would not be ok either because cat IS NOT a Character
+	// (the are not even related)
+	// Character* d= new cat();
+
+	a->sayHello("student");
+
+	b->sayHello("student");
+
+	c->sayHello("student");
+}
